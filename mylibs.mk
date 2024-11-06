b2dad694fee12c22f07b81abc050e25da1629cd6
Patrick <ptc48@cornell.edu>
2024-11-06 16:16:30 -0500

    Removing MSDK dependecy from CMSIS/DSP by changing settings and mylibs.mk

--- mylibs.mk
+++ mylibs.mk
@@ -113,7 +113,35 @@ $(info $(shell tput setaf 4)Information: Loading peripheral driver from: $(PERIP
 
 #======================== CUSTOM ========================
 DSP_DRIVER_DIR := $(CURDIR)/CMSIS/5.9.0/DSP/
-include $(DSP_DRIVER_DIR)/CMSIS-DSP.mk
+# Include paths...
+# DSP files
+IPATH += $(DSP_DRIVER_DIR)/Include
+# Some newer CMSIS5 core include files, such as cmsis_compiler.h, etc.
+IPATH += $(DSP_DRIVER_DIR)/Core/Include
+
+# TODO: Add target check for M3 core micros
+# Add processor flag for arm_math.h
+PROJ_CFLAGS+=-DARM_MATH_CM4
+
+# Tell core_cm4.h that our CPU has an FPU by defining __FPU_PRESENT
+PROJ_CFLAGS+=-D__FPU_PRESENT
+
+# Where to find the DSP library file
+PROJ_LDFLAGS += -L$(DSP_DRIVER_DIR)/Lib
+
+# Where to find the DSP library file
+PROJ_LDFLAGS += -L$(CMSIS_ROOT)/5.9.0/DSP/Lib
+
+ifeq "$(MFLOAT_ABI)" ""
+$(warning ***The 'MFLOAT_ABI' Makefile variable is not set!***  Using softfp CMSIS-DSP instructions by default.)
+endif
+
+# Link against hard or soft fp
+ifeq "$(MFLOAT_ABI)" "hard"
+PROJ_LIBS += arm_cortexM4lf_math
+else
+PROJ_LIBS += arm_cortexM4l_math
+endif
 $(info $(shell tput setaf 3)Information: Loading DSP driver from: $(DSP_DRIVER_DIR)$(shell tput sgr0))
 #=======================================================
 
